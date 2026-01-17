ff9259cb29ea6239bdf86dc6e6148686bda18e08
fyder9 <gossettifederico778@gmail.com>
2026-01-17 19:19:33 +0100

    completed raw inference for SCRFD model detector class

--- deface/scrfd_detector.py
+++ deface/scrfd_detector.py
@@ -1,25 +1,12 @@
-import os
+
 import cv2
 import numpy as np 
 
-
-def ensure_rgb(self, img):
-
-        """Convert input image to RGB if it is in RGBA or L format"""
-        if img.ndim == 2:  # 1-channel grayscale -> RGB
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        elif img.shape[2] == 4:  # 4-channel RGBA -> RGB
-            img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
-        return img
-
-
-class SCRFDdetector:
+class SCRFDdetector: #Low-level SCRFD ONNX runtime wrapper
     def __init__(self, model_path: str, device: str = "cpu", cap_long_side: int = 1920, override_execution_provider: str = None):
         import onnxruntime
-        from .scrfd import SCRFD
-
+        #from .scrfd import SCRFD
         self.cap_long_side = cap_long_side  # Maximum long side length for input frames
-
         providers = onnxruntime.get_available_providers()   # Get list of available providers & choose one
          # If no override, use all available providers
         if override_execution_provider is None:
@@ -29,27 +16,43 @@ class SCRFDdetector:
                 ort_providers = [override_execution_provider]
             else:
                 raise ValueError(f"Requested execution provider '{override_execution_provider}' is not available. Available providers: {providers}")
-        self.model = SCRFD(model_path, device=device, providers=ort_providers)
+        self.sess = onnxruntime.InferenceSession(model_path, providers=ort_providers)
+        self.input_name = self.sess.get_inputs()[0].name
+        self.output_names = [output.name for output in self.sess.get_outputs()]
 
-    def resize_frame(self, frame: np.ndarray) -> np.ndarray:
+    def ensure_rgb(self,img: np.ndarray) -> np.ndarray:
+            
+            if img.ndim == 2:  # 1-channel grayscale -> RGB
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            elif img.shape[2] == 4:  # 4-channel RGBA -> RGB
+                img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+            return img
+    
+    def resize_frame(self, frame: np.ndarray) -> tuple[np.ndarray, float, float]:
         #Resize input frame to have its long side equal to cap_long_side while maintaining aspect ratio
         h, w = frame.shape[:2]
-        if max(h, w) > self.cap_long_side:
+        longside = max(h, w)
+        if longside > self.cap_long_side:
             scale = self.cap_long_side / max(h, w)
             new_w = int(w * scale)
             new_h = int(h * scale)
-            frame = cv2.resize(frame, (new_w, new_h)) 
-        #returning resized frame else original frame
-        return frame
+            frame = cv2.resize(frame, (new_w, new_h))
+        else:
+            return frame, 1.0, 1.0
+        #returning resized or original frame
+        return frame, new_w / w, new_h / h
     
-    def rgb_to_bgr(self, img: np.ndarray) -> np.ndarray:
-        """Convert input image from RGB to BGR format"""
-        img_bgr=cv2.cvtColor(img, cv2.COLOR_RGB2BGR).astype(np.float32)
+    def preprocess(self, img_rgb: np.ndarray) -> np.ndarray:
+        #TODO:Convert input from RGB to BGR based on SCRFD requirements
+        img_bgr=cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR).astype(np.float32)
         img_bgr = (img_bgr -127.5) / 128.0
+        img_bgr = np.transpose(img_bgr, (2, 0, 1))  # HWC to CHW
+        img_bgr = np.expand_dims(img_bgr, axis=0)  # Add batch dimension
         return img_bgr
     
-    def infer(self, frame: np.ndarray):
-        frame_resized = self.resize_frame(frame)
-        #img_bgr = self.rgb_to_bgr(frame_resized)
-        bboxes, kpss = self.model.detect(frame_resized, max_num=0, metric='default', iou_thresh=0.5, score_thresh=0.5)
-        return bboxes, kpss
+    def infer(self, frame: np.ndarray): #Perform raw inference on input frame
+        frame = self.ensure_rgb(frame)
+        frame, scale_w, scale_h = self.resize_frame(frame)
+        blob = self.preprocess(frame)
+        outputs = self.sess.run(self.output_names, {self.input_name: blob})
+        return outputs, scale_w, scale_h
